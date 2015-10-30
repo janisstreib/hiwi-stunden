f919aa0722d4bdd8656e144473e330ac04a96fca
Janis Streib <janis@dogcraft.de>
2015-10-30 18:03:20 +0100

    ADD: more ui and basic profile page

--- hiwi_portal/urls.py
+++ hiwi_portal/urls.py
@@ -6,5 +6,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 urlpatterns = [
+    url(r'^profile/', views.profile, name='profile'),
     url(r'^$', views.index, name='index'),
 ]
--- hiwi_portal/views.py
+++ hiwi_portal/views.py
@@ -5,3 +5,9 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     context = {"user":request.user}
     return render(request, 'hiwi_portal.html', context)
+
+
+@login_required
+def profile(request):
+    context = {"user":request.user}
+    return render(request, 'profile.html', context)

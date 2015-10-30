4822d89e1e8c019f64ac2d6402e5295aa06eec83
Janis Streib <janis@dogcraft.de>
2015-10-30 17:35:04 +0100

    ADD: login/logout button

--- hiwi_portal/views.py
+++ hiwi_portal/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    context = {}
+    context = {"user":request.user}
     return render(request, 'hiwi_portal.html', context)
